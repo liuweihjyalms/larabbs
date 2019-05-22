59deacc91e9140ea02314d4daa495bd5fdc53180
liuwei <liuweihjyalms@163.com>
2019-05-22 10:23:04 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://weibo.com/2024774757/profile?topnav=1&wvr=6&is_all=1&display=0&retcode=6102" target="_blank">刘伟HJY_141_896_240</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:liuweihjyalms@163.com">联系我们</a></p>
+  </div>
+</footer>
