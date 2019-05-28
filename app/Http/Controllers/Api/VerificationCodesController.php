50e78fe225335cb86fb74ecb15425ea36574eb82
liuwei <liuweihjyalms@163.com>
2019-05-28 18:14:58 +0000

    add captcha,图片验证码,安装 gregwar/captcha,新建控制器和表单验证类

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers\Api;
 
-use Overtrue\EasySms\EasySms;
 use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
 use App\Http\Requests\Api\VerificationCodeRequest;
 
 class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captchaData = \Cache::get($request->captcha_key);
 
-        //除了正式环境外，其他环境，默认不真实发送短信，短信验证码默认为 1234
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
+
         if (!app()->environment('production')) {
             $code = '1234';
         } else {
@@ -25,7 +36,7 @@ class VerificationCodesController extends Controller
                 ]);
             } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
                 $message = $exception->getException('yunpian')->getMessage();
-                return $this->response->errorInternal($message ?: '短信发送异常');
+                return $this->response->errorInternal($message ?? '短信发送异常');
             }
         }
 
@@ -33,6 +44,8 @@ class VerificationCodesController extends Controller
         $expiredAt = now()->addMinutes(10);
         // 缓存验证码 10分钟过期。
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
