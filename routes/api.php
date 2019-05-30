a78f389a01bd0cba29592b13f36ff936d892dd53
liuwei <liuweihjyalms@163.com>
2019-05-30 13:44:28 +0000

    api/user, 获取个人信息,数据结构Fractal,创建 UserTransformer

--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,15 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api) {
     $api->group([
         'middleware' => 'api.throttle',
         'limit' => config('api.rate_limits.sign.limit'), 
         'expires' => config('api.rate_limits.sign.expires'),
     ], function($api) {
+        // 游客可以访问的接口
         // 图片验证码
         $api->post('captchas', 'CaptchasController@store')
             ->name('api.captchas.store');
@@ -44,6 +46,14 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
+
     });
 });
 
