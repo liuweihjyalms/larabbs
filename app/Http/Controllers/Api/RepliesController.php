7fd29092b5466f88f44a9213d56a3a3fdc390df8
liuwei <liuweihjyalms@163.com>
2019-06-02 03:15:23 +0000

    replies index,回复列表, N+1 问题,持续关注这个 Bug，等待 Bug 解决,安装回 Dingo 2.0.0 以下的版本,关闭 Dingo 的预加载，手动处理预加载的问题

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -4,11 +4,37 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Topic;
 use App\Models\Reply;
+use App\Models\User;
+use Illuminate\Http\Request;
 use App\Transformers\ReplyTransformer;
 use App\Http\Requests\Api\ReplyRequest;
 
 class RepliesController extends Controller
 {
+    public function index(Topic $topic, Request $request)
+    {
+        app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+        $replies = $topic->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load($request->include);
+        }
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+
+    public function userIndex(User $user, Request $request)
+    {
+        app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+        $replies = $user->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load($request->include);
+        }
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+
     public function store(ReplyRequest $request, Topic $topic, Reply $reply)
     {
         $reply->content = $request->content;
